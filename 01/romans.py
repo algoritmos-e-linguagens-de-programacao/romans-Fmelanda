5aa372422316ff78b9d4484bfd68e493e7be7b21
Felipe Melanda <felipeemelanda@gmail.com>
2024-06-05 20:12:55 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -15,6 +15,35 @@ def int_to_roman(num):
 
 
 
-def roman_to_int(s):
+def roman_to_int(roman):
     # Implemente sua função aqui
+
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for numeral in reversed(roman):
+        value = roman_numerals[numeral]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
+
+
+roman_input = input("Digite um número romano: ").upper()
+
+
+print("O número decimal equivalente é:", roman_to_int(roman_input))
     pass
